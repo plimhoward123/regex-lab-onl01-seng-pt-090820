c33474a9478c7ff6274225664b40ff154c913889
Plimon Howard V <plimon.howard@gmail.com>
2020-09-23 19:04:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   tempArry = []
-  if (text.match(/[^un]/) && text.match(/[$ing]/))
+  if (text.match(/[\Aun]/) && text.match(/[\zing]/))
     tempArry << text
     puts text
   end
