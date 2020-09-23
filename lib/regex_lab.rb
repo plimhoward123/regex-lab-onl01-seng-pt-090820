49fbd43661274e6c0c16e6cc69dd422718af9d7a
Plimon Howard V <plimon.howard@gmail.com>
2020-09-23 17:49:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  #return text.scan(/[])
+  tempArry = []
+  if (text.scan(/[un]) && text.scan(/[ing]))
+    tempArry << text
+  end
+  return tempArry
 end
 
 def words_five_letters_long(text)
