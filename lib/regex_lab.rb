20bf772921ce880bc2883aae00ded5a2196a8a8e
Plimon Howard V <plimon.howard@gmail.com>
2020-09-23 17:50:48 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -6,6 +6,7 @@ def words_starting_with_un_and_ending_with_ing(text)
   tempArry = []
   if (text.match(/[un]/) && text.match(/[ing]/))
     tempArry << text
+    puts text
   end
   return tempArry
 end
