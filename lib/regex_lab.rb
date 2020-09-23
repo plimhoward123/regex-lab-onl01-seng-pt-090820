d10c2f88b669f582af8a2537385bb79bca1a0872
Plimon Howard V <plimon.howard@gmail.com>
2020-09-23 19:18:03 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   tempArry = []
-  if (text.starts_with?("un") && text.ends_with?("ing")
+  if (text.starts_with?("un") && text.ends_with?("ing"))
     puts text
   end
 end
