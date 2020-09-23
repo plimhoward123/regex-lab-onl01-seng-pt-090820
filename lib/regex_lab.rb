a93150de8364ca15570196e59f9450b496c077f3
Plimon Howard V <plimon.howard@gmail.com>
2020-09-23 17:39:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-   !!word.capitalize.match(/[AEIOU]/)
+   if (word.capitalize.scan(/[AEIOU]/) != nil)
+     return true
+   else
+     return false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
